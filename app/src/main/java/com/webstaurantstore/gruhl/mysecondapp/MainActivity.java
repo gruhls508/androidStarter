c265dec529e60873014d068b43bc799a5057c911
Glen Ruhl <gruhl@Mac-OSx-1010-Base-2.local>
2015-02-19 11:13:04 -0500

    books return prog dialog added

--- app/src/main/java/com/webstaurantstore/gruhl/mysecondapp/MainActivity.java
+++ app/src/main/java/com/webstaurantstore/gruhl/mysecondapp/MainActivity.java
@@ -1,6 +1,10 @@
 package com.webstaurantstore.gruhl.mysecondapp;
 
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.ActionBarActivity;
@@ -10,12 +14,19 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 
 
@@ -24,10 +35,16 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
     TextView mainTextView;
     Button mainButton;
     EditText mainEditText;
-    ListView mainListVew;
-    ArrayAdapter mArrayAdapter;
+    ListView mainListView;
+    JSONAdapter mJSONAdapter;
     ArrayList mNameList = new ArrayList();
     ShareActionProvider mShareActionProvider;
+    ProgressDialog mDialog;
+
+    private static final String PREFS = "prefs";
+    private static final String PREF_NAME = "name";
+    private static final String QUERY_URL = "http://openlibrary.org/search.json?q=";
+    SharedPreferences mSharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,21 +52,27 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         mainTextView = (TextView) findViewById(R.id.main_textview);
-        mainTextView.setText("Set in javaaa");
 
         mainButton = (Button) findViewById(R.id.main_button);
         mainButton.setOnClickListener(this);
 
         mainEditText = (EditText) findViewById(R.id.main_editText);
 
-        mainListVew = (ListView) findViewById(R.id.main_listview);
+        mainListView = (ListView) findViewById(R.id.main_listview);
 
-        mArrayAdapter = new ArrayAdapter(this,
-                        android.R.layout.simple_list_item_1,
-                        mNameList);
+        mainListView.setOnItemClickListener(this);
 
-        mainListVew.setAdapter(mArrayAdapter);
-        mainListVew.setOnItemClickListener(this);
+        displayWelcome();
+
+        // 10. Create a JSONAdapter for the ListView
+        mJSONAdapter = new JSONAdapter(this, getLayoutInflater());
+
+    // Set the ListView to use the ArrayAdapter
+        mainListView.setAdapter(mJSONAdapter);
+
+        mDialog = new ProgressDialog(this);
+        mDialog.setMessage("Searching for Book");
+        mDialog.setCancelable(false);
     }
 
     @Override
@@ -78,19 +101,86 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
         }
     }
 
-
     @Override
     public void onClick(View v) {
-        mainTextView.setText(mainEditText.getText().toString()
-                        + " is doing the android shuffle.");
-        mNameList.add(mainEditText.getText().toString());
-        mArrayAdapter.notifyDataSetChanged();
-        setShareIntent();
+        queryBooks(mainEditText.getText().toString());
+        mDialog.show();
     }
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
-        Log.d("omgwtfbbq android", position + ": " + mNameList.get(position));
+    }
+
+    public void displayWelcome() {
+        mSharedPreferences = getSharedPreferences(PREFS, MODE_PRIVATE);
+        String name = mSharedPreferences.getString(PREF_NAME, "");
+
+        if (name.length() > 0) {
+
+            Toast.makeText(this, "Welcome back, " + name + ".", Toast.LENGTH_LONG).show();
+        } else {
+            AlertDialog.Builder alert = new AlertDialog.Builder(this);
+            alert.setTitle("WHAT UP");
+            alert.setMessage("who are you?");
+
+            final EditText input = new EditText(this);
+            alert.setView(input);
+
+            alert.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int whichButton) {
+
+                    String inputName = input.getText().toString();
+                    SharedPreferences.Editor e = mSharedPreferences.edit();
+                    e.putString(PREF_NAME, inputName);
+                    e.commit();
+
+                    Toast.makeText(getApplicationContext(), "Wilkömmen, " + inputName + ".",
+                            Toast.LENGTH_LONG).show();
+                }
+            });
+
+            alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+
+                public void onClick(DialogInterface dialog, int whichButton) {
+                }
+            });
+
+            alert.show();
+        }
+    }
+
+
+    private void queryBooks(String searchString) {
+        String urlString = "";
+        try {
+            urlString = URLEncoder.encode(searchString, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+
+            e.printStackTrace();
+            Toast.makeText(this, "Error: " + e.getMessage(), Toast.LENGTH_LONG).show();
+        }
+        AsyncHttpClient client = new AsyncHttpClient();
+
+        client.get(QUERY_URL + urlString,
+                new JsonHttpResponseHandler() {
+
+                    @Override
+                    public void onSuccess(JSONObject jsonObject) {
+                        mDialog.dismiss();
+                        Toast.makeText(getApplicationContext(), "Success!", Toast.LENGTH_LONG).show();
+                        mJSONAdapter.updateData(jsonObject.optJSONArray("docs"));
+                    }
+
+                    @Override
+                    public void onFailure(int statusCode, Throwable throwable, JSONObject error) {
+                            mDialog.dismiss();
+                            Toast.makeText(getApplicationContext(), "Error: " + statusCode + " " +
+                                                throwable.getMessage(), Toast.LENGTH_LONG).show();
+
+                                  Log.e("omg android", statusCode + " " + throwable.getMessage());
+                    }
+                });
     }
 }
